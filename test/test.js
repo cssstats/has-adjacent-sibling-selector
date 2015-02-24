10290b4ab43a6d40e5a473bbc7bc8a24e1871b81
John Otander <johnotander@gmail.com>
2015-02-23 21:19:16 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var hasAdjacentSiblingSelector = require('..');
+
+var adjacentSiblingSelectors = [
+  'li + li',
+  '#foo .bar .baz + .baz'
+];
+
+var noAdjacentSiblingSelectors = [
+  'foo',
+  '#bar',
+  'li > li'
+];
+
+describe('has-adjacent-sibling-selector', function() {
+
+  it('should do something return true when an adjacent sibling selector exists', function() {
+    adjacentSiblingSelectors.forEach(function(selector) {
+      assert.ok(hasAdjacentSiblingSelector(selector));
+    });
+  });
+
+  it('should do something return false when no adjacent sibling selector exists', function() {
+    noAdjacentSiblingSelectors.forEach(function(selector) {
+      assert.ok(!hasAdjacentSiblingSelector(selector));
+    });
+  });
+});
